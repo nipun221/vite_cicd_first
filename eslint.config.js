f7039a60f02ede62842c216eb20d2dd66c2fe50e
nipun221 <nipunvats5@gmail.com>
2025-11-02 00:05:04 +0530

    added lint run and fixed eslint errors

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import vitest from "eslint-plugin-vitest-globals";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -13,6 +14,10 @@ export default defineConfig([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      reactHooks,
+      reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -26,4 +31,24 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+
+  {
+    files: [
+      "**/*.test.{js,jsx}",
+      "**/*.spec.{js,jsx}",
+      "src/__tests__/**/*.{js,jsx}",
+    ],
+    plugins: {
+      vitest,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals, // adds test, expect, describe, etc.
+      },
+    },
+    rules: {
+      // optional: allow dev deps in test files
+      "no-unused-expressions": "off",
+    },
+  },
+]);
